c06fbee79f77284196d8b330e26a03df9f3a42f7
YuHui <littlebang@outlook.com>
2015-07-22 14:44:16 +0800

    update fis

--- index.js
+++ index.js
@@ -7,64 +7,15 @@
 
 'use strict';
 
-var fis = module.exports = require('fis');
+var fis = module.exports = require('fis3');
 
-/**
- * update notifier service
- */
-var updateNotifier = require('update-notifier');
-
-/**
- * package info
- */
-var pkg = fis.util.readJSON(__dirname + '/package.json');
-
-// Checks for available update and returns an instance
-var notifier = updateNotifier({pkg: pkg});
-
-// Notify using the built-in convenience method
-notifier.notify();
-
-/**
- * prefixes
- * @type {string[]}
- */
-fis.require.prefixes = ['sulky', 'fis'];
-
-/**
- * cli name
- * @type {string}
- */
+fis.require.prefixes.unshift('sulky');
 fis.cli.name = 'sulky';
-
-/**
- * cli package info
- */
-fis.cli.info = pkg;
-
+fis.cli.info = require('./package.json');
 /**
  * commands
  * @type {string[]}
  */
 fis.cli.help.commands = ['release', 'server'];
 
-
-/**
- * cli version
- * @type {*|exports|module.exports}
- */
 fis.cli.version = require('./lib/version');
-
-/**
- * 合并默认配置
- * @type {*|exports|module.exports}
- */
-var defaultConfig = require('./lib/config.js');
-
-/**
- * merge default config
- */
-fis.config.merge(defaultConfig);
-
-
-
